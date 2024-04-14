c4dea44734b981762389c5d1ddb6a2978c1d7607
jan <jan@netshake.com>
2024-04-14 23:56:49 +0200

    add nerdfont glyphs to status messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::env::{var, current_dir, args};
+use std::env::{args, current_dir, var};
+
 fn main() {
     let mut stack: Vec<String> = if var("tagger_stack").is_ok() {
-            var("tagger_stack")
+        var("tagger_stack")
             .unwrap()
             .split(":")
             .filter_map(|s| {
@@ -17,21 +18,22 @@ fn main() {
     };
 
     let path = current_dir()
-    .expect("Can't access current directory")
-    .to_str()
-    .expect("Can't convert current directory to string")
-    .to_owned();
+        .expect("Can't access current directory")
+        .to_str()
+        .expect("Can't convert current directory to string")
+        .to_owned();
 
     match args()
-    .skip(1)
-    .next()
-    .expect("No argument provided")
-    .as_str() {
+        .skip(1)
+        .next()
+        .expect("No argument provided")
+        .as_str()
+    {
         "tag" => {
             stack.push(path.to_owned());
             println!("export tagger_stack={}", stack.join(":"));
-            print!("echo \"Tagged {}\"", path);
-        },
+            print!("echo \"\u{ea66} Tagged {}\"", path);
+        }
         "pop" => {
             if stack.is_empty() {
                 print!("echo \"Can't pop empty stack\"");
@@ -40,9 +42,8 @@ fn main() {
             let popped = stack.pop().unwrap();
             println!("cd {}", &popped);
             println!("export tagger_stack={}", stack.join(":"));
-            print!("echo \"Popped {}\"", &popped);
+            print!("echo \"\u{f0e2} Popped {}\"", &popped);
         }
-        ,
-        _ => panic!("Invalid argument provided")
+        _ => panic!("Invalid argument provided"),
     };
 }
