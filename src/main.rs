8a7855fe34d9543fc2baa99355418620d98aa132
jan <jan@netshake.com>
2024-04-21 19:57:46 +0200

    stack -> registers

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 use std::env::{args, current_dir, var};
-
+use std::process::exit;
 fn main() {
-    let mut stack: Vec<String> = if var("tagger_stack").is_ok() {
-        var("tagger_stack")
+    let mut registers: Vec<String> = if var("tagger_registers").is_ok() {
+        var("tagger_registers")
             .unwrap()
             .split(":")
-            .filter_map(|s| {
-                if s.is_empty() {
-                    None
-                } else {
-                    Some(s.to_owned())
-                }
-            })
+            .map(|s| s.to_owned())
             .collect()
     } else {
-        Vec::new()
+        vec![String::from(""); 10]
     };
 
     let path = current_dir()
@@ -22,38 +16,100 @@ fn main() {
         .to_str()
         .expect("Can't convert current directory to string")
         .to_owned();
-
-    match args()
-        .skip(1)
-        .next()
+    let args: Vec<String> = args().skip(1).collect();
+    if args.len() > 2 {
+        println!("echo \"Too many arguments provided\"");
+        exit(1);
+    }
+    match args.get(0)
         .expect("No argument provided")
         .as_str()
     {
         "tag" => {
-            stack.push(path.to_owned());
-            println!("export tagger_stack={}", stack.join(":"));
-            print!("echo \"\u{ea66} Tagged: {}\"", path);
-        }
-        "pop" => {
-            if stack.is_empty() {
-                print!("echo \"Can't pop empty stack\"");
-                std::process::exit(0);
+            if let Some(arg) = args.get(1) {
+               if let Ok(reg_i) = arg.parse::<usize>() {
+                    if reg_i < registers.len() {
+                        registers[reg_i].push_str(&path);
+                        println!("echo \"Pushing into register {reg_i}: {}\"", &registers[reg_i]);
+                        println!("export tagger_registers={}", registers.join(":"));
+                        exit(0);
+                    } else {
+                        println!("echo \"Index out of bounds\"");
+                        exit(1);
+                    }
+               } else {
+                     println!("echo \"Can't parse argument {arg} as index\"");
+                     exit(1);
+               }
             }
-            let popped = stack.pop().unwrap();
-            println!("cd {}", &popped);
-            println!("export tagger_stack={}", stack.join(":"));
-            print!("echo \"\u{f0e2} Popped: {}\"", &popped);
+            let mut found_empty = false;
+            for (i, reg) in registers.iter_mut().enumerate() {
+                if reg.is_empty() {
+                    found_empty = true;
+                    reg.push_str(&path);
+                    println!("echo \"Pushing {path} into empty register {i}\"");
+                    break;
+                }
+            }
+            if !found_empty {
+                println!("echo \"Overwriting last register with {path}\"");
+                let last_i = registers.len() - 1;
+                registers[last_i] = path;
+            }
+            println!("export tagger_registers={}", registers.join(":"));
+        }
+        "move" => {
+            if registers.iter().all(|s| s.is_empty()) {
+                print!("echo \"All registers are empty\"");
+                exit(0);
+            }
+            if let Some(arg) = args.get(1) {
+               if let Ok(reg_i) = arg.parse::<usize>() {
+                    if reg_i < registers.len() {
+                        if registers[reg_i].is_empty() {
+                            println!("echo \"\u{f057} Register {reg_i} is empty\"");
+                            exit(1);
+                        }
+                        println!("cd {}", &registers[reg_i]);
+                        println!("echo \"Moving into register {reg_i}: {}\"", &registers[reg_i]);
+                        exit(0);
+                    } else {
+                        println!("echo \"Index out of bounds\"");
+                        exit(1);
+                    }
+               } else {
+                     println!("echo \"Can't parse argument {arg} as index\"");
+                     exit(1);
+               }
+            }
+            // cd into the last non-empty register
+            for (i,reg) in registers.iter().enumerate().rev() {
+                if !reg.is_empty() {
+                        println!("cd {}", reg);
+                        println!("echo \"Moving into register {i}: {reg}\"");
+                        exit(0);
+                }
+            }
         }
         "list" => {
-            if stack.is_empty() {
-                println!("echo \"\u{f51e} Current stack is empty\"")
+            if registers.iter().all(|reg| reg.is_empty()) {
+                println!("echo \"\u{f51e} Current registers are empty\"")
             } else {
-                println!("echo \"\u{f51e} Current stack:\n{}\"", stack.join("\n"))
+                println!("echo \"\u{f51e} Current registers:\n{}\"", registers
+                .iter()
+                .enumerate()
+                .filter_map(|(i, val) |{
+                    if val.is_empty() {
+                        None
+                    } else {
+                        Some(format!("[{}] {}", i, val))
+                    }
+                }).collect::<Vec<String>>().join("\n"))
             }
         }
         "purge" => {
-            println!("unset tagger_stack");
-            println!("echo \"\u{f057} Purged stack\"");
+            println!("unset tagger_registers");
+            println!("echo \"\u{f057} Purged registers\"");
         }
         _ => panic!("Invalid argument provided"),
     };
